e3ab117d275a4ed6424a9c873cf8d25c38e0877a
Sarthak Goyal <goyalsarthak3332gmail.com>
2023-01-31 22:47:20 +0530

    Sample Class Creation, 31-Jan

---
+++ Sampleclass.rb
@@ -0,0 +1,21 @@
+#classs Person
+#end
+#p=Person.new
+
+
+
+#class Person
+   # def initialize(name,age)
+   #     @name=name
+  #      @age=age 
+  #  end 
+#end
+
+#p=Person.new("Sarthak",21)
+
+class Person
+    def name
+    @name
+    end
+
+puts p.name="Sarthak Goyal"
