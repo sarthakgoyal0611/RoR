d84325a5f77837b65cd189f1385029098654a852
Sarthak Goyal <goyalsarthak3332gmail.com>
2023-01-31 22:04:21 +0530

    Function Creation, String Concatination, 31-Jan

--- First.rb
+++ First.rb
@@ -1,1 +1,17 @@
-//Commments
+# Function Creation
+def myPuts (msg)
+puts msg 
+end 
+myPuts('Hi Sarthak') 
+# Puts return nill
+# In Ruby Last line also get return
+x=10
+puts x.class 
+
+puts "i" *4     #Executed Succesfully
+# puts 4*"i"    This thoows Error
+
+#String Concatenation
+s='Hello'
+puts s+' Sarthak'
+
